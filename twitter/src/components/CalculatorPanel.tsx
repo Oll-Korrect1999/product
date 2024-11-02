fed56cc6600d39b28c109e55d2879243eec45f9f
T-Kaneniwa <deuberg962@outlook.jp>
2024-11-02 23:51:46 +0900

    電卓構造をテーブル化

--- twitter/src/components/CalculatorPanel.tsx
+++ twitter/src/components/CalculatorPanel.tsx
@@ -6,68 +6,109 @@ type CalculatorBoardType = {
 };
 const CalculatorBoard = ({ value, buttonHandler }: CalculatorBoardType) => {
   return (
-    <div className={styles.board}>
-      <div>{value}</div>
-      <div>
-        <button className={styles.button} onClick={() => buttonHandler("1")}>
-          1
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("2")}>
-          2
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("3")}>
-          3
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("+")}>
-          +
-        </button>
-      </div>
-      <div>
-        <button className={styles.button} onClick={() => buttonHandler("4")}>
-          4
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("5")}>
-          5
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("6")}>
-          6
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("-")}>
-          -
-        </button>
-      </div>
-      <div>
-        <button className={styles.button} onClick={() => buttonHandler(".")}>
-          .
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("7")}>
-          7
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("8")}>
-          8
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("9")}>
-          9
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("*")}>
-          *
-        </button>
-      </div>
-      <div>
-        <button className={styles.button} onClick={() => buttonHandler("AC")}>
-          AC
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("0")}>
-          0
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("=")}>
-          =
-        </button>
-        <button className={styles.button} onClick={() => buttonHandler("÷")}>
-          ÷
-        </button>
-      </div>
-    </div>
+    <table className={styles.board}>
+      {/* 数値表示部分 */}
+      <tr>
+        <td colSpan={4}>{value}</td>
+      </tr>
+      {/* 1段目 */}
+      <tr>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("1")}>
+            1
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("2")}>
+            2
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("3")}>
+            3
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("+")}>
+            +
+          </button>
+        </td>
+      </tr>
+      {/* 2段目 */}
+      <tr>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("4")}>
+            4
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("5")}>
+            5
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("6")}>
+            6
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("-")}>
+            -
+          </button>
+        </td>
+      </tr>
+      {/* 3段目 */}
+      <tr>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler(".")}>
+            .
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("7")}>
+            7
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("8")}>
+            8
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("9")}>
+            9
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("*")}>
+            *
+          </button>
+        </td>
+      </tr>
+      {/* 最下段 */}
+      <tr>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("AC")}>
+            AC
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("0")}>
+            0
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("=")}>
+            =
+          </button>
+        </td>
+        <td>
+          <button className={styles.button} onClick={() => buttonHandler("÷")}>
+            ÷
+          </button>
+        </td>
+      </tr>
+    </table>
   );
 };
 
