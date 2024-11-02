974396e571bd5a93f2b5fca467339a09008e07ee
Oll-Korrect1999 <xkmy2tri@gmail.com>
2024-11-02 20:50:46 +0900

    割り算追加しました！！！！！！

--- twitter/src/components/ButtonPanel.tsx
+++ twitter/src/components/ButtonPanel.tsx
@@ -1,33 +1,33 @@
 export default function ButtonPanel(props: {
-    buttonHandler: (code: string) =>void; //各ボタンが押下された際にこの関数が呼び出される
+  buttonHandler: (code: string) => void; //各ボタンが押下された際にこの関数が呼び出される
 }) {
-    return<div>
-        <div>
-            <button onClick={() => props.buttonHandler("1")}>1</button>
-            <button onClick={() => props.buttonHandler("2")}>2</button>
-            <button onClick={() => props.buttonHandler("3")}>3</button>
-            <button onClick={() => props.buttonHandler("+")}>+</button>
-        </div>
-        <div>
-            <button onClick={() => props.buttonHandler("4")}>4</button>
-            <button onClick={() => props.buttonHandler("5")}>5</button>
-            <button onClick={() => props.buttonHandler("6")}>6</button>
-            <button onClick={() => props.buttonHandler("-")}>-</button>
-        </div>
-        <div>
-            <button onClick={() => props.buttonHandler("7")}>7</button>
-            <button onClick={() => props.buttonHandler("8")}>8</button>
-            <button onClick={() => props.buttonHandler("9")}>9</button>
-            <button onClick={() => props.buttonHandler("*")}>*</button>
-        </div>
-        <div>
-            <button onClick={() => props.buttonHandler("AC")}>AC</button>
-            <button onClick={() => props.buttonHandler("0")}>0</button>
-            <button onClick={() => props.buttonHandler("=")}>=</button>
-            <button onClick={() => props.buttonHandler("/")}>/</button>
-        </div>
-        
-
-
-    </div>;
+  return (
+    <div>
+      <div>
+        <button onClick={() => props.buttonHandler("1")}>1</button>
+        <button onClick={() => props.buttonHandler("2")}>2</button>
+        <button onClick={() => props.buttonHandler("3")}>3</button>
+        <button onClick={() => props.buttonHandler("+")}>+</button>
+      </div>
+      <div>
+        <button onClick={() => props.buttonHandler("4")}>4</button>
+        <button onClick={() => props.buttonHandler("5")}>5</button>
+        <button onClick={() => props.buttonHandler("6")}>6</button>
+        <button onClick={() => props.buttonHandler("-")}>-</button>
+      </div>
+      <div>
+        <button onClick={() => props.buttonHandler(".")}>.</button>
+        <button onClick={() => props.buttonHandler("7")}>7</button>
+        <button onClick={() => props.buttonHandler("8")}>8</button>
+        <button onClick={() => props.buttonHandler("9")}>9</button>
+        <button onClick={() => props.buttonHandler("*")}>*</button>
+      </div>
+      <div>
+        <button onClick={() => props.buttonHandler("AC")}>AC</button>
+        <button onClick={() => props.buttonHandler("0")}>0</button>
+        <button onClick={() => props.buttonHandler("=")}>=</button>
+        <button onClick={() => props.buttonHandler("÷")}>÷</button>
+      </div>
+    </div>
+  );
 }
